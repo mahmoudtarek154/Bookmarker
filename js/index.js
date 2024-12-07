50558cda450baa62ade1e28556aa04f60f9d861c
mahmoud154 <mahmoudtarek15444@gmail.com>
2024-12-07 16:13:53 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,115 +1,77 @@
-var SiteName = document.getElementById("SiteName");
-var SiteURL = document.getElementById("SiteURL");
-var fixidi = document.getElementById("fixidi")
+let inputname = document.querySelector(".inputname");
+let inputemail = document.querySelector(".inputemail");
+let inputpassword = document.querySelector(".inputpassword");
+let signinemail = document.querySelector(".signinemail");
+let signinpassword = document.querySelector(".signinpassword");
+let welcomeh1 = document.querySelector(".welcomeh1");
 
-var SiteList = [];
+let loginarr = [];
 if (localStorage.getItem("list")) {
-  SiteList = JSON.parse(localStorage.getItem("list"));
-  siteInformationDisplay();
+  loginarr = JSON.parse(localStorage.getItem("list"));
 }
 
-function siteInformationAdd() {
-  var siteInformation = {
-    Name: SiteName.value,
-    URL: SiteURL.value,
-  };
-  SiteList.push(siteInformation);
-  siteInformationDisplay();
-  localStorage.setItem("list", JSON.stringify(SiteList));
-}
-function siteInformationDisplay() {
-  var temp = "";
-  for (var i = 0; i < SiteList.length; i++) {
-    temp +=
-      `   <tr>
-            <td>${i + 1}</td>
-            <td>${SiteList[i].Name}</td>
-            <td>
-             <a  href="
-             ${
-               SiteList[i].URL.startsWith("https://")
-                 ? SiteList[i].URL
-                 : "https://" + SiteList[i].URL
-             }
-             " target="_blank">  <button   style="background-color:  var(--main-color); border: 2px solid  var(--main-color) ;" id="buttonVisit" class="btn btn-primary"> <i class="fa-solid fa-plane-departure me-2"></i>Visit</button></a>
-            </td>
-            <td>
-             <button " onclick="siteInformationDelet(` +
-      i +
-      `)" style="background-color: #dc2f2f; border: 2px solid #dc2f2f ;" id="buttonVisit" type="button" class="btn btn-primary"> <i class="fa-solid fa-trash me-2"></i>Delet</button>
-            </td>
-          </tr>`;
-  }
-  document.getElementById("TableBody").innerHTML = temp;
-}
+function loginAdd() {
+  let regNameinput = /[A-Za-z]{3,16}/;
+  let regEmailinput = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+  let regPasswordinput =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
 
-
-function siteInformationDelet(x) {
-  SiteList.splice(x, 1);
-  siteInformationDisplay();
-
-  localStorage.setItem("list", JSON.stringify(SiteList));
-}
-
-function orangecolor (){
-  document.documentElement.style.cssText="  --main-color : #ff894c "
-}
-function redcolor (){
-  document.documentElement.style.cssText="  --main-color : #ffb6b9 "
-}
-function greencolor (){
-  document.documentElement.style.cssText="  --main-color : #00adb5 "
-}
-function bluecolor (){
-  document.documentElement.style.cssText="  --main-color : #2772db "
-}
-function lemoncolor (){
-  document.documentElement.style.cssText="  --main-color : #a9c52f "
-}
-function purplecolor (){
-  document.documentElement.style.cssText="  --main-color : #9d53c3 "
-}
-function pinkcolor (){
-  document.documentElement.style.cssText="  --main-color : #ff347f "
-}
-
-
-function siteInformationAdd() {
-  var urlPattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([/\w \.-]*)*\/?$/;
-var SiteNameinput = /[A-Za-z]{3,16}/;
-  
-if(!urlPattern.test(SiteURL.value) &&!SiteNameinput.test(SiteName.value)) {
-
-
-  alert("Please enter a valid name and a valid URL ");
-  return; 
-
-
-} else if(!urlPattern.test(SiteURL.value)) {
-    alert("Please enter a valid URL.");
-  
-    return; 
-  }else if (!SiteNameinput.test(SiteName.value))  {
-
-    alert("Please enter a valid name (3-16 charachter) ");
-
-    return; 
-
+  if (
+    !regNameinput.test(inputname.value) &&
+    !regEmailinput.test(inputemail.value) &&
+    !regPasswordinput.test(inputpassword.value)
+  ) {
+    alert("Please enter a valid email and a valid password ");
+    return;
   }
 
-  var siteInformation = {
-    Name: SiteName.value,
-    URL: SiteURL.value,
+  let logininfo = {
+    name: inputname.value,
+    email: inputemail.value,
+    password: inputpassword.value,
   };
 
-  SiteList.push(siteInformation);
-  siteInformationDisplay();
-  localStorage.setItem("list", JSON.stringify(SiteList));
+  loginarr.push(logininfo);
+  localStorage.setItem("list", JSON.stringify(loginarr));
 }
 
 
 
+function signin() {
+  let isValid = false;
+
+  loginarr.some((data) => {
+    if (
+      data.email === signinemail.value &&
+      data.password === signinpassword.value
+    ) {
+      isValid = true;
+      localStorage.setItem("currentUser", data.name);
+      window.location = "main.html";
+      return true;
+    }
+  });
+
+  if (!isValid) {
+    alert(
+      "Invalid email or password. If you don't have an account, please sign up."
+    );
+  }
+}
 
 
+function logout() {
+  window.location = "index.html";
+}
+function signup() {
+  window.location = "home.html";
+}
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  let currentUser = localStorage.getItem("currentUser");
+
+  if (currentUser) {
+    welcomeh1.textContent = `Welcome ${currentUser}`;
+  }
+});
